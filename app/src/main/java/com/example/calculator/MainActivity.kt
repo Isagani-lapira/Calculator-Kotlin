5014ff031ff3e9dfbb84e2d6bea079616e664203
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-26 08:58:59 +0800

    Listener for buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.calculator
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,26 +9,48 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var context: Context = this
-
-
     private var text: String? = ""
+    private var value: String? = ""
     private lateinit var tvText: TextView
+    private lateinit var tvPreview: TextView
+    private lateinit var btnAc: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initialize()
+        listener()
     }
+
+    private fun listener() {
+        btnAc.setOnClickListener{
+            tvPreview.text = ""
+            tvText.text = ""
+        }
+    }
+
 
     private fun initialize() {
         tvText = findViewById(R.id.tvValue)
+        tvPreview = findViewById(R.id.tvPreview)
+        btnAc = findViewById(R.id.btnAc)
+
     }
+
+
 
     fun buttonClick(view:View){
         text  += (view as Button).text.toString()
         tvText.text = text
     }
 
+    fun operationButton(view: View) {
+        value += text + (view as Button).text.toString()
+        tvPreview.text = value
+        text=""
+    }
+
 }
+
+
