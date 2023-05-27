7b575cedd8c8d6ee479746ff8879631a06a7ea93
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-27 18:38:28 +0800

    Making String expression into mathematical expression

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.ExpressionBuilder
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var text: String? = ""
-    private var value: String? = ""
+    private var text = ""
+    private var value =  ""
     private lateinit var tvText: TextView
     private lateinit var tvPreview: TextView
     private lateinit var btnAc: Button
@@ -24,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun listener() {
+        //clear the input
         btnAc.setOnClickListener{
             tvPreview.text = ""
             tvText.text = ""
@@ -31,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    //reference the views needed
     private fun initialize() {
         tvText = findViewById(R.id.tvValue)
         tvPreview = findViewById(R.id.tvPreview)
@@ -41,14 +45,38 @@ class MainActivity : AppCompatActivity() {
 
 
     fun buttonClick(view:View){
+        //when clicked the button it will appear on the preview of text
         text  += (view as Button).text.toString()
         tvText.text = text
     }
 
     fun operationButton(view: View) {
+        /* when one of the operators has been clicked
+        it indicates that new number will be added
+        show to the preview the number before the mathematical operator
+        */
         value += text + (view as Button).text.toString()
         tvPreview.text = value
+
+        tvText.text = "0" //make it zero after the operator has been clicked
         text=""
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun calculate(view: View) {
+        value += tvText.text
+        tvPreview.text = "$value="
+        text=""
+
+        val expression = tvPreview.text.toString().trimEnd('=')
+        val result = evaluateMathExpression(expression)
+        tvText.text = result.toString()
+    }
+
+    //a function that turning String into Mathematical expression
+    private fun evaluateMathExpression(expression: String): Double {
+        val exp = ExpressionBuilder(expression).build()
+        return exp.evaluate()
     }
 
 }
